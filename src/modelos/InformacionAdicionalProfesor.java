3fece902c326b395f8d9c444cdc2613ae585a317
Valeria Barzola <valeriadba.28@gmail.com>
2020-08-12 23:06:23 -0500

    Resolviendo code smell lazy class y data class

--- src/modelos/InformacionAdicionalProfesor.java
+++ src/modelos/InformacionAdicionalProfesor.java
@@ -1,8 +1,40 @@
 package modelos;
 
 public class InformacionAdicionalProfesor {
-    public int añosdeTrabajo;
-    public String facultad;
-    public double BonoFijo;
+    private int añosdeTrabajo;
+    private String facultad;
+    private double BonoFijo;
+    
+    public double calcularSueldo(Profesor prof){
+        double sueldo=0;
+        sueldo= añosdeTrabajo*600 + BonoFijo;
+        return sueldo;
+    }  
+
+    public int getAñosdeTrabajo() {
+        return añosdeTrabajo;
+    }
+
+    public String getFacultad() {
+        return facultad;
+    }
+
+    public double getBonoFijo() {
+        return BonoFijo;
+    }
+
+    public void setAñosdeTrabajo(int añosdeTrabajo) {
+        this.añosdeTrabajo = añosdeTrabajo;
+    }
+
+    public void setFacultad(String facultad) {
+        this.facultad = facultad;
+    }
+
+    public void setBonoFijo(double BonoFijo) {
+        this.BonoFijo = BonoFijo;
+    }
+    
+    
     
 }
