3dc88ccd4fa339db18826949f31e0ee29a144718
JaimePizarro <japibaro@espol.edu.ec>
2020-08-12 23:35:45 -0500

    Self Encapsulate Field en Estudiante Nota

--- src/modelos/EstudianteNota.java
+++ src/modelos/EstudianteNota.java
@@ -18,6 +18,49 @@ public class EstudianteNota {
     
     
     public void calcularNotaTotal(){
-        notaTotal = (notaInicial.calcularNota() + notaFinal.calcularNota())/2;
+        setNotaTotal((getNotaInicial().calcularNota() + getNotaFinal().calcularNota())/2);
     }
+
+    public Nota getNotaInicial() {
+        return notaInicial;
+    }
+
+    public void setNotaInicial(Nota notaInicial) {
+        this.notaInicial = notaInicial;
+    }
+
+    public Nota getNotaFinal() {
+        return notaFinal;
+    }
+
+    public void setNotaFinal(Nota notaFinal) {
+        this.notaFinal = notaFinal;
+    }
+
+    public double getNotaTotal() {
+        return notaTotal;
+    }
+
+    public void setNotaTotal(double notaTotal) {
+        this.notaTotal = notaTotal;
+    }
+
+    public Estudiante getEstudiante() {
+        return estudiante;
+    }
+
+    public void setEstudiante(Estudiante estudiante) {
+        this.estudiante = estudiante;
+    }
+
+    public Paralelo getParalelo() {
+        return paralelo;
+    }
+
+    public void setParalelo(Paralelo paralelo) {
+        this.paralelo = paralelo;
+    }
+    
+    
+    
 }
